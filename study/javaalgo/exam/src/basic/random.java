44499ee85ade4cd380a3298d8fb9222385f86a2a
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-11 12:42:01 +0900

    랜덤으로 정수10개의 값을 갖는 배열을 만들어 최댓값과 최소값 출력

--- study/javaalgo/exam/src/basic/random.java
+++ study/javaalgo/exam/src/basic/random.java
@@ -1,5 +1,27 @@
 package basic;
+import java.util.Random;
 
 public class random {
-	//문제
+	//랜덤을 정수10개의 값을 갖는 배열을 만들어 최댓값과 최솟값을 같이 출력하기
+	
+	public static void main(String[] args) {
+		int[] ran = new int[10];
+	
+		//배열값 초기화
+	for(int i=0; i<ran.length; i++) {
+		ran[i] = (int)(Math.random()*10);
+		}
+	
+	int max = ran[0];
+	int min = ran[0];
+	for(int i=0; i<ran.length; i++) {
+		if(max <ran[i]) {
+			max = ran[i];
+		}
+		if(min > ran[i]) {
+			min = ran[i];
+		}
+	}
+	System.out.println("최댓값은 : " + max + "최소값은 : " + min);
+	}
 }
