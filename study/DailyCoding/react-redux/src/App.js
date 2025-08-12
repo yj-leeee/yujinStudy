8f0957eecb5a4f172a7ed26772ee9ef266bec666
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-12 15:48:04 +0900

    AddNumber 만들기

--- study/DailyCoding/react-redux/src/App.js
+++ study/DailyCoding/react-redux/src/App.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
 
+class AddNumber extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Add Number</h1>
+        <input type="button" value="+"></input>
+        <input type="text" value="0"></input>
+      </div>
+    )
+  }
+}
+
 class AddNumberRoot extends Component{
   render(){
     return(
       <div>
         <h1>Add Number Root</h1>
+        <AddNumber></AddNumber>
       </div>
     )
   }
