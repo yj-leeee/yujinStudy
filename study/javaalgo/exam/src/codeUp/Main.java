bab656369ace458ae6bcc1506afa454b811e156b
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-12 12:50:22 +0900

    코드업 1024문제부터

--- study/javaalgo/exam/src/codeUp/Main.java
+++ study/javaalgo/exam/src/codeUp/Main.java
@@ -1,10 +1,16 @@
 package codeUp;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.printf("Hello%nWorld");
-
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.nextLine();
+		String[] parts = a.split("\\.");
+		
+		System.out.printf("%d.%02d.%02d",Integer.parseInt(parts[0]),Integer.parseInt(parts[1]),Integer.parseInt(parts[2]));
 	}
 
 }
