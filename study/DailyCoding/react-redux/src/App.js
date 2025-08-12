4f0b112149cb8adf7af1a9a5e3fc085e87b0c7d3
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-12 16:07:06 +0900

    마찬가지로 Display도 해준다

--- study/DailyCoding/react-redux/src/App.js
+++ study/DailyCoding/react-redux/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import AddNumberRoot from './Components/AddNumberRoot';
+import DisplayNumberRoot from './Components/DisplayNumberRoot';
 
 function App() {
   return (
@@ -12,26 +13,7 @@ function App() {
   );
 }
 
-class DisplayRoot extends Component{
-  render(){
-    return(
-      <div>
-        <h1>Display Number</h1>
-        <input type="text" value="0" readOnly></input>
-      </div>
-    )
-  }
-}
 
-class DisplayNumberRoot extends Component{
-  render(){
-    return(
-      <div>
-        <h1>Display Number Root</h1>
-        <DisplayRoot></DisplayRoot>
-      </div>
-    )
-  }
-}
+
 
 export default App;
---
+++ study/DailyCoding/react-redux/src/Components/DisplayNumberRoot.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import DisplayRoot from '../Components/DisplayRoot';
+
+export default class DisplayNumberRoot extends Component{
+    render(){
+    return(
+        <div>
+            <h1>Display Number Root</h1>
+            <DisplayRoot></DisplayRoot>
+        </div>
+        )
+    }
+}
