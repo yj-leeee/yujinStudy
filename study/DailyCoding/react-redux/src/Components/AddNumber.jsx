f8adc8d52175b8f0a819d2b85415ffb01947e353
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-12 16:36:01 +0900

    안에 숫자를 바꾸면 **상위컴포넌트로 올라갈 수 있게** 설정  this.props.onClick을 호출하면서 (this.state.size)값을 같이 전달

--- study/DailyCoding/react-redux/src/Components/AddNumber.jsx
+++ study/DailyCoding/react-redux/src/Components/AddNumber.jsx
@@ -6,7 +6,9 @@ render(){
     return(
     <div>
         <h1>Add Number</h1>
-        <input type="button" value="+"></input>
+        <input type="button" value="+" onClick={function(){
+            this.props.onClick(this.state.size);
+        }.bind(this)}></input>
         <input type="text" value={this.state.size} onChange={function(e){
             this.setState({size:e.target.value});
         }.bind(this)}></input>
