9707b0625f95e9464c591e94cd0d63d7140855a4
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-12 16:30:25 +0900

    AddNumber에서 *몇씩 더 할 것인가*를 ==size==로 표현  저 state는 어떤것에 영향을 받냐면 onChange안에 함수 이렇게 바뀐 size가 바뀌어야 하니 value를 수정

--- study/DailyCoding/react-redux/src/Components/AddNumber.jsx
+++ study/DailyCoding/react-redux/src/Components/AddNumber.jsx
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 
 export default class AddNumber extends Component{
+    state = {size:1}
 render(){
     return(
     <div>
         <h1>Add Number</h1>
         <input type="button" value="+"></input>
-        <input type="text" value="0"></input>
+        <input type="text" value={this.state.size} onChange={function(e){
+            this.setState({size:e.target.value});
+        }.bind(this)}></input>
     </div>
     )
     }
