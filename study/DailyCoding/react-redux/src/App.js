4aeee4d5ebc03032388bcdf6ec01e93d2d087849
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-12 15:53:19 +0900

    DisplayNumber 만들기 복사해서 이름만 고쳐줌. display만 하면되기 때문에 ==readOnly==로 처리

--- study/DailyCoding/react-redux/src/App.js
+++ study/DailyCoding/react-redux/src/App.js
@@ -29,8 +29,31 @@ function App() {
     <div className="App">
       <h1>Root</h1>
       <AddNumberRoot></AddNumberRoot>
+      <DisplayNumberRoot></DisplayNumberRoot>
     </div>
   );
+}
+
+class DisplayRoot extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Display Number</h1>
+        <input type="text" value="0" readOnly></input>
+      </div>
+    )
+  }
+}
+
+class DisplayNumberRoot extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Display Number Root</h1>
+        <DisplayRoot></DisplayRoot>
+      </div>
+    )
+  }
 }
 
 export default App;
