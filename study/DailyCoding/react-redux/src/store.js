4c7a6fa84aace170594f651e0de916a9cfb531a8
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-12 17:05:46 +0900

    redux dev tools 기능을 사용하기 위해 store 두번째 인자에 `window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()` 넣어줌

--- study/DailyCoding/react-redux/src/store.js
+++ study/DailyCoding/react-redux/src/store.js
@@ -2,4 +2,4 @@ import { createStore } from 'redux';
 
 export default createStore(function(state, action){
     return state;
-})
+},window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
