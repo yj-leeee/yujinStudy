baabc52d44294ac57aea0ea16569bcaee5143a4e
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-12 16:01:53 +0900

    이제 App.js에서 AddNumberRoot를 사용하기 위해 import 해준다

--- study/DailyCoding/react-redux/src/App.js
+++ study/DailyCoding/react-redux/src/App.js
@@ -1,28 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-
-class AddNumber extends Component{
-  render(){
-    return(
-      <div>
-        <h1>Add Number</h1>
-        <input type="button" value="+"></input>
-        <input type="text" value="0"></input>
-      </div>
-    )
-  }
-}
-
-class AddNumberRoot extends Component{
-  render(){
-    return(
-      <div>
-        <h1>Add Number Root</h1>
-        <AddNumber></AddNumber>
-      </div>
-    )
-  }
-}
+import AddNumberRoot from './Components/AddNumberRoot';
 
 function App() {
   return (
