36c596a4cfe5cc1fa313f0975d1c19e501bed2de
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-12 15:43:24 +0900

    클래스형 컴포넌트로 해줌

--- study/DailyCoding/react-redux/src/App.js
+++ study/DailyCoding/react-redux/src/App.js
@@ -1,9 +1,21 @@
+import React, {Component} from 'react';
 import './App.css';
+
+class AddNumberRoot extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Add Number Root</h1>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <h1>Root</h1>
+      <AddNumberRoot></AddNumberRoot>
     </div>
   );
 }
