01903fd53da425a4faa72f78065dc3ab0dc25cab
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-12 16:13:38 +0900

    3.2 App.js가 state를 가지기 위해 **새로운 방식**인 ==class==형태로 바꿈

--- study/DailyCoding/react-redux/src/App.js
+++ study/DailyCoding/react-redux/src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import AddNumberRoot from './Components/AddNumberRoot';
 import DisplayNumberRoot from './Components/DisplayNumberRoot';
 
-function App() {
-  return (
+class App extends Component() {
+  render(){
+    return (
     <div className="App">
       <h1>Root</h1>
       <AddNumberRoot></AddNumberRoot>
       <DisplayNumberRoot></DisplayNumberRoot>
     </div>
   );
+  }
 }
 
 
