69bd00931a0a00c282722e5ae9af7e0bcc23dc89
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-13 15:43:12 +0900

    wrapping에 state와 constructore 가져오기

--- study/DailyCoding/react-redux/src/Containers/DisplayNumber.jsx
+++ study/DailyCoding/react-redux/src/Containers/DisplayNumber.jsx
@@ -3,6 +3,13 @@ import React, { Component } from "react";
 import DisplayRoot from "../Components/DisplayRoot";
 
 export default class extends Component{
+    state = {number:store.getState().number}
+    constructor(props){
+        super(props);
+        store.subscribe(function(){
+            this.setState({number:store.getState().number});
+        }.bind(this))
+    }
     render(){
         return <DisplayRoot></DisplayRoot>
     }
