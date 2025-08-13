ff67b9f7215c2ded9a6f8213025612c54fb9d9a3
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-13 14:39:53 +0900

    기존에 AddNumber에 있었던 ` store.dispatch({type:'INCREMENT', size:this.state.size})` 코드를 wrapping 컴포넌트로 옮기고 그 위치에는 `this.props.onClick(this.state.size);` 을 넣어 onClick을 했을때 값을 this.state.size로 준다

--- study/DailyCoding/react-redux/src/Components/AddNumber.jsx
+++ study/DailyCoding/react-redux/src/Components/AddNumber.jsx
@@ -7,7 +7,7 @@ render(){
     <div>
         <h1>Add Number</h1>
         <input type="button" value="+" onClick={function(){
-            store.dispatch({type:'INCREMENT', size:this.state.size})
+            this.props.onClick(this.state.size);
         }.bind(this)}></input>
         <input type="text" value={this.state.size} onChange={function(e){
             this.setState({size:Number(e.target.value)});
