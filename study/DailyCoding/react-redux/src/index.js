b80c445fb4e150f2f1572ff577a2d17aabdb67df
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-13 16:33:47 +0900

    그러고 App 컴포넌트를 Provider컴포넌트로 감싸준다

--- study/DailyCoding/react-redux/src/index.js
+++ study/DailyCoding/react-redux/src/index.js
@@ -8,7 +8,9 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> 
+    <Provider>
+      <App />
+    </Provider> 
   </React.StrictMode>
 );
 
