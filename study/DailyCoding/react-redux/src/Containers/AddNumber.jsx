f5a086a0765c120e08250411f7f2bd87227f77c4
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-13 16:50:10 +0900

    AddNumber도 해줌

--- study/DailyCoding/react-redux/src/Containers/AddNumber.jsx
+++ study/DailyCoding/react-redux/src/Containers/AddNumber.jsx
@@ -1,11 +1,14 @@
-import React, { Component } from "react";
 import AddNumber from "../Components/AddNumber";
-import store from "../store";
+import { connect } from "react-redux";
 
-export default class extends Component{
-    render(){
-        return <AddNumber onClick={function(size){
-            store.dispatch({type:'INCREMENT', size:size})
-        }.bind(this)}></AddNumber>
-    }
-}
+export default connect()(AddNumber);
+// import store from "../store";
+// import React, { Component } from "react";
+
+// export default class extends Component{
+//     render(){
+//         return <AddNumber onClick={function(size){
+//             store.dispatch({type:'INCREMENT', size:size})
+//         }.bind(this)}></AddNumber>
+//     }
+// }
