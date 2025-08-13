c6a4f20f99ae7b142c941b415e26001e2f5c5a9f
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-13 16:28:29 +0900

    connect()를 실행하면 **함수를 반환** (이 함수는 우리가 감싸고 싶은 컴포넌트를 인자로 받는다) 그 반환된 함수를 **다시 실행**해서 컴포넌트를 넘기면, Redux Store와 연결된 ==새로운 래핑(wrapped)컴포넌트==를 만들어 준다

--- study/DailyCoding/react-redux/src/Containers/DisplayNumber.jsx
+++ study/DailyCoding/react-redux/src/Containers/DisplayNumber.jsx
@@ -1,16 +1,19 @@
-import store from "../store";
-import React, { Component } from "react";
 import DisplayRoot from "../Components/DisplayRoot";
+import {connect} from 'react-redux';
+export default connect()(DisplayRoot);
 
-export default class extends Component{
-    state = {number:store.getState().number}
-    constructor(props){
-        super(props);
-        store.subscribe(function(){
-            this.setState({number:store.getState().number});
-        }.bind(this))
-    }
-    render(){
-        return <DisplayRoot number={this.state.number}></DisplayRoot>
-    }
-}
+// import store from "../store";
+// import React, { Component } from "react";
+
+// export default class extends Component{
+//     state = {number:store.getState().number}
+//     constructor(props){
+//         super(props);
+//         store.subscribe(function(){
+//             this.setState({number:store.getState().number});
+//         }.bind(this))
+//     }
+//     render(){
+//         return <DisplayRoot number={this.state.number}></DisplayRoot>
+//     }
+// }
