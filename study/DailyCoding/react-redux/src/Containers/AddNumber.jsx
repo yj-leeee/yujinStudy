a6b4394b0b7c3ff3610bd0c13f3a3ef8b3a81434
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-13 14:30:44 +0900

    Wrapping하려는 컴포넌트를 만들고 render를 하는데 return값을 AddNumber로 한다

--- study/DailyCoding/react-redux/src/Containers/AddNumber.jsx
+++ study/DailyCoding/react-redux/src/Containers/AddNumber.jsx
@@ -1,1 +1,8 @@
+import React, { Component } from "react";
 import AddNumber from "../Components/AddNumber";
+
+export default class extends Component{
+    render(){
+        return <AddNumber></AddNumber>
+    }
+}
