6a7a6b134fa1757d780f0d90c4a6a5c4d1692700
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-13 14:40:51 +0900

    기존에 있던 컴포넌트에서 store 없애도 됨

--- study/DailyCoding/react-redux/src/Components/AddNumber.jsx
+++ study/DailyCoding/react-redux/src/Components/AddNumber.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import store from '../store'
 export default class AddNumber extends Component{
     state = {size:1}
 render(){
--- study/DailyCoding/react-redux/src/Containers/AddNumber.jsx
+++ study/DailyCoding/react-redux/src/Containers/AddNumber.jsx
@@ -5,7 +5,7 @@ import store from "../store";
 export default class extends Component{
     render(){
         return <AddNumber onClick={function(){
-
+            store.dispatch({type:'INCREMENT', size:this.state.size})
         }.bind(this)}></AddNumber>
     }
 }
