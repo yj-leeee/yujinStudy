a6a42124c212e26242ea3c2b5f0cb6ab8e4c6367
DESKTOP-N2A6ATC\user <asas87189@gmail.com>
2025-08-13 15:40:46 +0900

    wrapping하기 위해 DisplayNumber.jsx 만들기

---
+++ study/DailyCoding/react-redux/src/Containers/DisplayNumber.jsx
@@ -0,0 +1,9 @@
+import store from "../store";
+import React, { Component } from "react";
+import DisplayRoot from "../Components/DisplayRoot";
+
+export default class extends Component{
+    render(){
+        return <DisplayRoot></DisplayRoot>
+    }
+}
