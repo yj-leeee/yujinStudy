0c636ca5a20e4bbbb7c658dd4122958ea741e6e9
yj-leeee <asas87189@gmail.com>
2025-08-20 15:16:52 +0900

    props를 통해 속성값을 전달해서 배경바꾸기

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -3,23 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Change color="yellow"></Change>
   );
+}
+function Change(props){
+  return(
+    <button onClick={()=>{
+    document.body.style.backgroundColor= props.color;
+    }}>배경색 변경</button> 
+  )
 }
 
 export default App;
