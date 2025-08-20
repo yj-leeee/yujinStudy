eeeb67e5eeee0172082cc3c7f5076b645f0b34fd
yj-leeee <asas87189@gmail.com>
2025-08-20 15:35:40 +0900

    mode로 나눠서

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [mode, setMode] = useState('DEFAULT');
+  let color = null;
+  if(mode === 'DEFALUT'){
+    color = "white";
+  }else if(mode === "CHANGE"){
+    color = "blue";
+  }
   return (
-    <Change color="yellow"></Change>
+    <Change color={color} setMode={setMode}></Change>
   );
 }
 function Change(props){
   return(
-    <button onClick={()=>{
-    document.body.style.backgroundColor= props.color;
-    }}>배경색 변경</button> 
+    <button
+    onClick={()=>{
+      props.setMode('CHANGE');
+      document.body.style.backgroundColor = props.color
+      }}>
+        배경색 변경</button>
   )
 }
 
