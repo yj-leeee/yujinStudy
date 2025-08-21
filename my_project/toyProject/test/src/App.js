1e50102b36333de3e67d0bddba65536ad3cfd774
yj-leeee <asas87189@gmail.com>
2025-08-21 13:53:39 +0900

    1-1 props 문제 시작

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -3,26 +3,11 @@ import './App.css';
 import React, {useState} from 'react';
 
 function App() {
-  const [mode, setMode] = useState('DEFAULT');
-  let color = null;
-  if(mode === 'DEFALUT'){
-    color = "white";
-  }else if(mode === "CHANGE"){
-    color = "blue";
-  }
+  
   return (
-    <Change color={color} setMode={setMode}></Change>
+    <div></div>
   );
 }
-function Change(props){
-  return(
-    <button
-    onClick={()=>{
-      props.setMode('CHANGE');
-      document.body.style.backgroundColor = props.color
-      }}>
-        배경색 변경</button>
-  )
-}
+
 
 export default App;
