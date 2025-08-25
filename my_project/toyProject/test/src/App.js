db99751f5b1598a07f9ecb3fa80e743632099097
yj-leeee <asas87189@gmail.com>
2025-08-25 13:04:46 +0900

    2-3 간단한 회원가입 시뮬레이션 만들기

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -8,24 +8,10 @@ function App() {
 }
 
 function Name(){
-  const [name, setName] = useState("");
-  const [message, setMessage] = useState("이름을 입력해주세요");
+  
   
   return(
-    <div>
-      <input type='text' value={name}
-      onChange={(e)=>setName(e.target.value)}
-      placeholder='이름을 입력하세요'></input>
-      <button onClick={()=>{
-        if(name === ""){
-          setMessage('이름이 입력 되어있지 않습니다')
-        }else {
-          setMessage(`안녕하세요 ${name}님`)
-        }
-      }}>확인 버튼</button>
-      <p>{message}</p>
-      
-    </div>
+    
   )
 }
 
