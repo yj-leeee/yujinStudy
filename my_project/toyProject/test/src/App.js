9049a3f8e9ee265422cc7f31e0f729646307f8e4
yj-leeee <asas87189@gmail.com>
2025-08-25 13:58:31 +0900

    3-4 로그인 만들기

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -1,16 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import LoginCard from './Componetents/LoginCard';
+
 import { useState } from 'react';
 
 function App() {
   const [name, setName] = useState("");
   
   return (
-  <div>
-  <LoginCard setName={setName}></LoginCard>
-  <p>{name}님 안녕하세요</p>
-  </div>)
+  
 }
 
 
