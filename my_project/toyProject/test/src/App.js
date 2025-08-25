36091ef386526fb332ed1121e793c41eae89de43
yj-leeee <asas87189@gmail.com>
2025-08-25 15:15:54 +0900

    완성

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -5,11 +5,27 @@ import { useEffect, useState } from 'react';
 
 function App() {
   
-
+  const [show, setShow] = useState(false);
   return(
-    
+    <div>
+      <button onClick={()=>{setShow(!show)}}>버튼</button>
+      {show &&<Time />}
+    </div>
   );
 
+}
+function Time(){
+  const ti = new Date().toLocaleDateString();
+  useEffect(()=>{
+    console.log("컴포넌트가 나타났다");
+    
+    return()=>{
+      console.log("컴포넌트가 사라졌다");
+    }
+  },[])
+  return(
+    <p>{ti}</p>
+  )
 }
 
 
