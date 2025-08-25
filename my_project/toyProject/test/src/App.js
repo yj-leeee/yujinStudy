45c2ebf03b412a3920efe26222f6f18c941e5e0d
yj-leeee <asas87189@gmail.com>
2025-08-25 14:47:07 +0900

    4-1.2 타이머 컴포넌트 만들기

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -3,23 +3,8 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [count,setCount] = useState(0);
-
-  useEffect(()=>{
-    console.log('컴포넌트가 화면에 나타났습니다');
-    return ()=>{
-      console.log('컴포넌트가 화면에서 사라졌습니다.')
-    }
-  },[])
-  
-  useEffect(()=>{
-    console.log(`카운트 : ${count}`)
-  },[count])
-  return(<div>
-    <button onClick={()=>{
-      setCount(count+1);
-    }}>버튼</button>
-  </div>)
+ 
+    
 }
 
 
