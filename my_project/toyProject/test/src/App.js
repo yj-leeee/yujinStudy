1822a0e440dd3b222bbf792c37b19e6f531c4f2b
yj-leeee <asas87189@gmail.com>
2025-08-25 11:11:17 +0900

    완성

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
-import UserProfile from './components/UserProfile';
+
+import NoticeCard from './components/NoticeCard';
 
 
 function App() {
   
   return (
-    
+    <NoticeCard title={"공지사항"} writer={"관리자"} onCheck={()=>{console.log("공지사항")}}>서버 점검이 오늘 밤 10시에 있습니다.</NoticeCard>
   );
 }
 
