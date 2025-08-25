c14433112e4dc5d111696c723cbb5f75d828b00e
yj-leeee <asas87189@gmail.com>
2025-08-25 11:14:48 +0900

    2-1 좋아요 버튼 만들기 시작

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -8,7 +8,7 @@ import NoticeCard from './components/NoticeCard';
 function App() {
   
   return (
-    <NoticeCard title={"공지사항"} writer={"관리자"} onCheck={()=>{console.log("공지사항")}}>서버 점검이 오늘 밤 10시에 있습니다.</NoticeCard>
+  
   );
 }
 
