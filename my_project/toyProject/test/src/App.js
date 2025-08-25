b2cbd05591b1db957e9e40537e37061443b8d249
yj-leeee <asas87189@gmail.com>
2025-08-25 13:54:47 +0900

    완성

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginCard from './Componetents/LoginCard';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState("");
   
-  return <LoginCard></LoginCard>
+  return (
+  <div>
+  <LoginCard setName={setName}></LoginCard>
+  <p>{name}님 안녕하세요</p>
+  </div>)
 }
 
 
--- my_project/toyProject/test/src/Componetents/LoginCard.jsx
+++ my_project/toyProject/test/src/Componetents/LoginCard.jsx
@@ -1,5 +1,15 @@
 import { useState } from "react";
 
 export default function LoginCard(props){
-   
+    const [ input, setInput] = useState("");
+    return(
+        <div>
+            <input type="text" value={input} onChange={(e)=>{
+                setInput(e.target.value);
+            }}></input>
+    <button onClick={()=>{
+        props.setName(input);
+    }}>버튼</button>
+        </div>
+    )
 }
