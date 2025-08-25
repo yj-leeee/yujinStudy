631953962d0038a2ae787877876228fb27fef834
yj-leeee <asas87189@gmail.com>
2025-08-25 14:33:51 +0900

    4-1useEffect 활용 1. 렌더링

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -1,14 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import LoginCard from './Componetents/LoginCard';
+
 
 function App() {
-  const [userName, setUserName] = useState("");
   
-  return (
-    <LoginCard userName={userName} setUserName={setUserName}></LoginCard>
-  )
   
 }
 
