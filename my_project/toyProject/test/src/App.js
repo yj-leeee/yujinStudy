fd2e5011d0ab28ab56916e98cc1c777b22d04c9e
yj-leeee <asas87189@gmail.com>
2025-08-25 13:10:33 +0900

    완성

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -4,14 +4,35 @@ import {useState} from 'react';
 
 function App() {
   
-  return <Name></Name>
+  return <Login></Login>
 }
 
-function Name(){
-  
+function Login(){
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [message, setMessage] = useState("");
   
   return(
-    
+    <div>
+      이름: <input type='text' value={name} onChange={(e)=>{
+        setName(e.target.value);
+      }}></input><br />
+      이메일: <input type='email' value={email} onChange={(e)=>{
+        setEmail(e.target.value);
+      }}></input><br />
+      비밀번호: <input type='password' value={pass} onChange={(e)=>{
+        setPass(e.target.value);
+      }}></input>
+    <button onClick={()=>{
+      if(name === "" || email === "" || pass === ""){
+        setMessage('제대로 입력해주세요')
+      }else{
+        setMessage(`안녕하세요 ${name}님 이메일 : ${email}`)
+      }
+    }}>클릭</button>
+      <p>{message}</p>
+    </div>
   )
 }
 
