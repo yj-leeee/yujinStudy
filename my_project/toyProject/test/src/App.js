d6a5fb62948b3f0514d015fee7a0659bc07e2061
yj-leeee <asas87189@gmail.com>
2025-08-25 11:01:14 +0900

    리액트 props활용문제 1-3

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -5,28 +5,9 @@ import UserProfile from './components/UserProfile';
 
 
 function App() {
-  const users = [
-    {
-    name: "이유진",
-    age: 23,
-    mbti: "INFP",
-    favoriteQuote: "조금은 느려도 괜찮아.",
-  },
-  {
-    name: "김수민",
-    age: 23,
-    mbti: "ENFJ",
-    favoriteQuote: "기다리는 것도 사랑이야.",
-  },
-  {
-    name: "박하나",
-    age: 22,
-    mbti: "ISTP",
-    favoriteQuote: "생각보다 단순하게.",
-  }
-  ]
+  
   return (
-    <UserProfile users={users}></UserProfile>
+    
   );
 }
 
