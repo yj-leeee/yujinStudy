69db5e783351fdf857a8f7a30474c64ad4344a06
yj-leeee <asas87189@gmail.com>
2025-08-25 11:23:44 +0900

    완성

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
-
-import NoticeCard from './components/NoticeCard';
-
+import {useState} from 'react';
 
 function App() {
   
-  return (
-  
-  );
+  return <LikeButton></LikeButton>
 }
 
+function LikeButton(){
+  const [Like, setLike] = useState(0);
+  return(
+    <div>
+      <p> 좋아요 수 : {Like} </p>
+    <button onClick={
+      ()=>{
+        setLike(Like+1);
+        console.log(Like);
+      }}>버튼</button>
+      </div>
+  )
+}
 
 export default App;
