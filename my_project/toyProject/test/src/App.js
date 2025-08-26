900ca8a63e05242045354b95ca9bc69b031d872d
yj-leeee <asas87189@gmail.com>
2025-08-26 10:34:24 +0900

    완성

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -4,11 +4,25 @@ import { useEffect, useRef, useState } from 'react';
 
 
 function App() {
- 
+  const inputRef = useRef();
+  const countRef = useRef(0);
+  const [dummy, setDummy] = useState(0);
+    useEffect(() => {
+        console.log(`입력횟수: ${countRef.current}`)
+      },[dummy])
 
   return(
     <div>
-      
+      <input onChange={()=>{
+        countRef.current += 1;
+        setDummy(dummy + 1)
+        
+      }}></input>
+    
+      <input ref={inputRef}></input>
+      <button onClick={()=>{
+        inputRef.current.focus();
+      }}></button>
     </div>
   );
 
