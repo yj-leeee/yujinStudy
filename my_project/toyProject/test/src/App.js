54cdd84ec33c83f9aee7bb4b38d11f9c6b675b95
yj-leeee <asas87189@gmail.com>
2025-08-26 09:50:33 +0900

    4.2 useRef 활용 1번 문제

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -5,28 +5,14 @@ import { useEffect, useState } from 'react';
 
 function App() {
   
-  const [show, setShow] = useState(false);
+
   return(
-    <div>
-      <button onClick={()=>{setShow(!show)}}>버튼</button>
-      {show &&<Time />}
-    </div>
+    
   );
 
 }
-function Time(){
-  const ti = new Date().toLocaleDateString();
-  useEffect(()=>{
-    console.log("컴포넌트가 나타났다");
-    
-    return()=>{
-      console.log("컴포넌트가 사라졌다");
-    }
-  },[])
-  return(
-    <p>{ti}</p>
-  )
-}
+
+
 
 
 export default App;
