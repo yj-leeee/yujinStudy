54a0cf70450f423cd6042e84fbfcc0ae4af729b7
yj-leeee <asas87189@gmail.com>
2025-08-26 15:04:08 +0900

    react redux로 다시 풀어보기

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import WrapPanel from './Container/WrapPanel';
-import WrapResult from './Container/WrapResult';
+
 
 
 function App() {
   
   return(
     <div>
-      <WrapPanel></WrapPanel>
-      <WrapResult></WrapResult>
+      
     </div>
   );
 
