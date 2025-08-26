ad58325a7da0039a0a027e6a84de5fe22304831a
yj-leeee <asas87189@gmail.com>
2025-08-26 09:59:31 +0900

    문제 2 두 개의 입력창과 버튼 만들기 시작

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -4,12 +4,11 @@ import { useEffect, useRef, useState } from 'react';
 
 
 function App() {
-  const input = useRef();
+  
 
   return(
     <div>
-      <input ref={input}></input>
-      <button onClick={()=>input.current.focus()}>입력창으로 이동</button>
+      
     </div>
   );
 
