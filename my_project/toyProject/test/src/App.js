0f5676466c8f9b963353f3a2581aa0a59df12e0f
yj-leeee <asas87189@gmail.com>
2025-08-26 09:56:46 +0900

    완성

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 
 function App() {
-  
+  const input = useRef();
 
   return(
-    
+    <div>
+      <input ref={input}></input>
+      <button onClick={()=>input.current.focus()}>입력창으로 이동</button>
+    </div>
   );
 
 }
