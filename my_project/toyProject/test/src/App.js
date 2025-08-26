95f0bc2ce8d3f5e2a818907c5b69c7ed406c9718
yj-leeee <asas87189@gmail.com>
2025-08-26 10:13:02 +0900

    완성

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -4,11 +4,20 @@ import { useEffect, useRef, useState } from 'react';
 
 
 function App() {
-  
+  const inputRef = useRef();
+  const countRef = useRef(0);
 
   return(
     <div>
-      
+      <input onKeyDown={(e)=>{
+        if(e.key === 'Enter'){
+        inputRef.current.focus()}}}></input>
+      <input ref={inputRef}></input>
+      <button onClick={()=>{
+        inputRef.current.focus()
+        countRef.current += 1
+        console.log(countRef.current);
+      }}>버튼</button>
     </div>
   );
 
