fcd9190047cfe608465a6a94f405baf1b2eb0b58
yj-leeee <asas87189@gmail.com>
2025-08-27 11:15:47 +0900

    react 2번문제 사용자 관리 애플리케이션 기존 redux

--- my_project/toyProject/test/src/App.js
+++ my_project/toyProject/test/src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import WrapPanel from './Wrapper/WrapPanel';
-import WrapResult from './Wrapper/WrapResult';
+
 
 function App() {
   
   return(
     <div>
-      <WrapPanel></WrapPanel>
-      <WrapResult></WrapResult>
+      
     </div>
   );
 
