9d0c54eb6018bed98e76e8e7cf4a4cd31a7b5a75
yj-leeee <asas87189@gmail.com>
2025-09-03 14:24:18 +0900

    react router dom 설치하고 broswer router(router) 설치하기

--- study/DailyCoding/react-router/src/index.js
+++ study/DailyCoding/react-router/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 function Home(){
   return(
