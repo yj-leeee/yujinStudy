fec7c4417687e274ff255e8d3c6582729f7e9658
yj-leeee <asas87189@gmail.com>
2025-09-03 15:14:42 +0900

    NavLink로 바꿔서 스타일 적용하기

--- study/DailyCoding/react-router/src/index.js
+++ study/DailyCoding/react-router/src/index.js
@@ -36,9 +36,9 @@ function App(){
     <div>
       <h1>React Router DOM example</h1>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/topics">Topics</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
       </ul>
 
       <Routes>
