904470023b822c2c4a34a2ebf864f22bee9fadc3
yj-leeee <asas87189@gmail.com>
2025-09-03 14:29:11 +0900

    이제 **Routing**의 가장 본질적인 작업인 *URL에 따른 적당한 컴포넌트가 이곳에 위치하도록 해보자* => 이걸 하기 위해 ==Route==라는 컴포넌트가 필요

--- study/DailyCoding/react-router/src/index.js
+++ study/DailyCoding/react-router/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function Home(){
   return(
