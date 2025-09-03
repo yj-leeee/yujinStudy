9d0056b75e33478a2a0e94ad29ef0fdee200211f
yj-leeee <asas87189@gmail.com>
2025-09-03 14:10:51 +0900

    직접 app 컴포넌트 만들기

--- study/DailyCoding/react-router/src/index.js
+++ study/DailyCoding/react-router/src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App(){
+  return(
+    <div>Hello React Router DOM</div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
