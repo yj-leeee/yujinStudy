d3597ac138105e2e9f5bec6d5f0722392d12f838
yj-leeee <asas87189@gmail.com>
2025-09-04 14:42:05 +0900

    nav부분 부품화 시키기

--- study/DailyCoding/react_ajax/src/App.js
+++ study/DailyCoding/react_ajax/src/App.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>WEB</h1>
-      <nav>
+class Nav extends Component {
+  render(){
+    return(<nav>
         <ul>
           <li><a href="1">HTML</a></li>
           <li><a href="2">CSS</a></li>
           <li><a href="3">JS</a></li>
         </ul>
-      </nav>
+      </nav>)
+  }
+}
+function App() {
+  return (
+    <div className="App">
+      <h1>WEB</h1>
+      <Nav></Nav>
       <article>
         <h2>Welcome</h2>
         Hello, React & Ajax
