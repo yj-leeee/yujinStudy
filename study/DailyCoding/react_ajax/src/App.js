06ae09581144d8ce14831c10ee7026936726cec3
yj-leeee <asas87189@gmail.com>
2025-09-04 14:31:45 +0900

    기본적인 UI 만들기기본적인 UI 만들기

--- study/DailyCoding/react_ajax/src/App.js
+++ study/DailyCoding/react_ajax/src/App.js
@@ -3,7 +3,18 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      
+      <h1>WEB</h1>
+      <nav>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JS</li>
+        </ul>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, React & Ajax
+      </article>
     </div>
   );
 }
