f26bd3ab4aa8d69712a1dc2b06dfaf98e2c40f48
yj-leeee <asas87189@gmail.com>
2025-09-04 15:00:31 +0900

    li에 key값을 li.id로 주기

--- study/DailyCoding/react_ajax/src/App.js
+++ study/DailyCoding/react_ajax/src/App.js
@@ -18,7 +18,7 @@ class Nav extends Component {
     var listTags = [];
     for(var i=0; i<this.state.list.length; i++){
       var li = this.state.list[i];
-      listTags.push(<li><a href={li.id}>{li.title}</a></li>)
+      listTags.push(<li key={li.id}><a href={li.id}>{li.title}</a></li>)
     }
     return(<nav>
         <ul>
