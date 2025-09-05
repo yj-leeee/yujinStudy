7a755c9cda6dcfd49952e74918e325f5e07a04d3
yj-leeee <asas87189@gmail.com>
2025-09-05 13:36:16 +0900

    Nav의 내부에서 state로 생성했던 li태그의 원천이 state가 아닌 props로 바뀌고, state는 필요없게 됨

--- study/DailyCoding/react_ajax/src/App.js
+++ study/DailyCoding/react_ajax/src/App.js
@@ -1,14 +1,10 @@
 import React, {Component} from "react";
 
 class Nav extends Component {
-  state = {
-    list:[]
-  }
-  
   render(){
     var listTags = [];
-    for(var i=0; i<this.state.list.length; i++){
-      var li = this.state.list[i];
+    for(var i=0; i<this.props.list.length; i++){
+      var li = this.props.list[i];
       listTags.push(<li key={li.id}>
         <a href={li.id} data-id={li.id} onClick={function(e){
           e.preventDefault();
