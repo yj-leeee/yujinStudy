9cfd61dddc4424b6a6724acd08dc0b7a761b3e76
yj-leeee <asas87189@gmail.com>
2025-10-20 10:06:37 +0900

    중단

--- my_project/frontmentor/profilecardcomponent11/src/App.js
+++ my_project/frontmentor/profilecardcomponent11/src/App.js
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div>
+      <Profile/>
     </div>
   );
+}
+
+function Profile(){
+  return(
+    <div id='container'>
+      <div id='img'></div>
+      <div id='profile'></div>
+      <div id='con2'>
+        <h4>Vicotor Crest <span className='gray'>26</span></h4>
+        <p className='gray'>London</p>
+        <hr id='hr'/>
+          <div id='text'>
+            <h4>80K</h4>
+            <h4>803K</h4>
+          </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
