2fb529de70c044453e97df441fbd345e04aeb27d
yj-leeee <asas87189@gmail.com>
2025-10-20 12:10:50 +0900

    로빈 2. 로그인 화면 만들기

--- my_project/robin/hellowrold1/app/_layout.tsx
+++ my_project/robin/hellowrold1/app/_layout.tsx
@@ -5,15 +5,6 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
 
   return (
-    <Stack>
-      <Stack.Screen name='index'
-      options={{
-        headerShown:false
-      }}/>
-      <Stack.Screen name='second'
-      options={{
-        headerShown:false
-      }}/>
-    </Stack>
+    <Stack />
   );
 }
