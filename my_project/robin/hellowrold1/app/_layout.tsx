a524325485ba71a42835791f1f3578ea991e6a0d
yj-leeee <asas87189@gmail.com>
2025-10-24 12:51:27 +0900

    로빈 5. 인스타그램 시작

--- my_project/robin/hellowrold1/app/_layout.tsx
+++ my_project/robin/hellowrold1/app/_layout.tsx
@@ -8,11 +8,7 @@ export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <Stack>
-        <Stack.Screen
-        name='index'
-        options={{
-          headerShown:false
-        }} />
+        
       </Stack>
     </SafeAreaProvider>
     
