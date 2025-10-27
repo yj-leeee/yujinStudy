4dea76860d4690f6efeab6bbe75bf86342a62691
yj-leeee <asas87189@gmail.com>
2025-10-27 13:33:57 +0900

    일단 완성

--- my_project/robin/hellowrold1/constants/thma.ts
+++ my_project/robin/hellowrold1/constants/thma.ts
@@ -1,2 +1,43 @@
 import { StyleSheet } from "react-native";
 
+export const styles = StyleSheet.create({
+    rsp:{
+        borderWidth:1,
+        borderColor:'black',  
+        marginVertical:50,
+        alignItems:'center'      
+    },
+    title:{
+        marginVertical:0
+    },
+    btn:{
+        position:'absolute',
+        bottom:100,
+        width:'100%',
+        flexDirection:'row',
+        justifyContent:'space-around'
+    }, 
+    btns:{
+        backgroundColor:'yellow',
+        width:50,
+        height:50,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    Display:{
+        flex:0.7,
+        width:'100%',
+        justifyContent:'space-around',
+        alignItems:'center',
+        flexDirection:'row',
+        paddingHorizontal: 20,
+    },
+    Computer:{
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    Me:{
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
