fb5fec6e5e012adedb67a7174a34daedfb6699ec
yj-leeee <asas87189@gmail.com>
2025-11-11 13:25:41 +0900

    프론트멘토 17 Intro component 시작

--- my_project/frontmentor/profilecardcomponent11/src/App.js
+++ my_project/frontmentor/profilecardcomponent11/src/App.js
@@ -6,15 +6,7 @@ import Header from './component/Header';
 
 function App() {
   return (
-    <div id='root'>
-        <Header />
-      <div id='contain'>
-        <div id='img'></div>
-        <div id='contain2'>
-          <Content />
-          <EmailInput/>
-        </div>
-      </div>
+    <div>
     </div>
   );
 }
--- my_project/frontmentor/profilecardcomponent11/src/component/Header.jsx
+++
@@ -1,9 +0,0 @@
-import logo from '../img/logo.svg'
-
-export default function Header(){
-    return(
-        <div style={{margin:"40px 20px"}}>
-            <img src={logo}/>
-        </div>
-    )
-}
