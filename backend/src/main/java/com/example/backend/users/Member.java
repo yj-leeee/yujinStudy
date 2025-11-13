3aa19ac05ea7ec71e13fbffd7ce03680b7555936
yj-leeee <asas87189@gmail.com>
2025-11-13 14:22:50 +0900

    엔터티 생성

---
+++ backend/src/main/java/com/example/backend/users/Member.java
@@ -0,0 +1,41 @@
+package com.example.backend.users;
+
+import java.time.LocalDateTime;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)
+	private String email;
+	
+	private String password;
+	private String name;
+	
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	
+	@PrePersist
+	public void onCreate() {
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+	}
+	
+	@PreUpdate
+	public void onUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+}
