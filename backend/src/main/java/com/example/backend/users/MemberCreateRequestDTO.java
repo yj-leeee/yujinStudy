b3b9949d66a9b9b9a4a53d4a551cfe4a74980805
yj-leeee <asas87189@gmail.com>
2025-11-13 15:09:30 +0900

    요청DTO 완성

--- backend/src/main/java/com/example/backend/users/MemberCreateRequestDTO.java
+++ backend/src/main/java/com/example/backend/users/MemberCreateRequestDTO.java
@@ -1,5 +1,7 @@
 package com.example.backend.users;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,8 +12,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 public class MemberCreateRequestDTO {
+	@NotBlank(message = "이름이 비었습니다.")
 	private String name;
+	
+	
+	//@Email에는 @NotBlank 필수 
+	//(없으면 공백도 허용이 되기 때문에 항상 두개를 조합해서 사용 할 것)
+	@Email(message = "이메일 형식이 아닙니다.") 
+	@NotBlank(message = "이메일이 비었습니다.")
 	private String email;
+	
+	@NotBlank(message = "비밀번호가 비었습니다.")
 	private String password;
 	
 	//DTO -> 엔터티 변환 메서드
