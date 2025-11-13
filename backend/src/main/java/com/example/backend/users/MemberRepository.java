9ae99d6d4d2865dd4a5ed6ea3e5bc59c4ac519e8
yj-leeee <asas87189@gmail.com>
2025-11-13 14:44:41 +0900

    리포지터리 생성

---
+++ backend/src/main/java/com/example/backend/users/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.backend.users;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface MemberRepository extends JpaRepository<Member, Long>{
+
+	Optional<Member> findByEmail(String email);
+	Optional<Member> eexistsByEmail(String email);
+}
