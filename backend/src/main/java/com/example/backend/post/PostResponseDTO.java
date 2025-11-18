e778abc633112b63cc38dc2a8aac658a20ae2e24
yj-leeee <asas87189@gmail.com>
2025-11-18 13:50:15 +0900

    4번 문제 responsedto 생성

---
+++ backend/src/main/java/com/example/backend/post/PostResponseDTO.java
@@ -0,0 +1,29 @@
+package com.example.backend.post;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class PostResponseDTO {
+
+	private Long id;
+	private String title;
+	private String content;
+	private String author;
+	private LocalDateTime createdAt;
+	
+	public static PostResponseDTO fromEntity(Post post) {
+		return PostResponseDTO.builder()
+				.id(post.getId())
+				.title(post.getTitle())
+				.content(post.getContent())
+				.author(post.getAuthor())
+				.createdAt(post.getCreatedAt())
+				.build();
+	}
+}
