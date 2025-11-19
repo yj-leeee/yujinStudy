9cb9ed0410a18772aba36510d38abb3bf53a55ea
yj-leeee <asas87189@gmail.com>
2025-11-19 13:57:45 +0900

    엔터티 생성

---
+++ backend/src/main/java/com/example/backend/list/Todo.java
@@ -0,0 +1,35 @@
+package com.example.backend.list;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Todo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String todo;
+
+	@Column(nullable = false)
+	private boolean checked; //할일 했는지 체크 (기본값은 false)
+	
+	public void update(String todo) {
+		this.todo = todo;
+	}
+	
+	public void toggleComplete() {
+		this.checked = !this.checked;
+	}
+}
