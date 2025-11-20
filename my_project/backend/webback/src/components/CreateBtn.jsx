9dae6cd48a229ecdf4127ddc544d6e1c7a4a3e59
yj-leeee <asas87189@gmail.com>
2025-11-20 14:18:04 +0900

    추가버튼 눌렀을때 펼쳐지게끔

--- my_project/backend/webback/src/components/CreateBtn.jsx
+++ my_project/backend/webback/src/components/CreateBtn.jsx
@@ -1,10 +1,30 @@
+import { useState } from 'react';
 import './CreateBtn.css';
 import { CiCirclePlus } from "react-icons/ci";
 
 export default function CreateBtn(){
+    const [rotate, setRotate] = useState(false);
+    const handleClick = () => {
+        setRotate(!rotate);
+    };
+
     return (
         <div id='createBtn'>
-            <button id='c_btn'> <CiCirclePlus size={50}/> </button>
+            <div className={`textBox ${rotate ? "open" : ""}`}>
+                <CreateText />
+            </div>
+            <button id='c_btn' onClick={handleClick}> 
+                <CiCirclePlus size={50} 
+                className={rotate ? "rotate" : "noRotate"}/> </button>
+        </div>
+    )
+}
+
+function CreateText(){
+    return(
+        <div id='text_div'>
+            <input placeholder='할일 추가' id='in_text'></input>
+            <button id="in_btn">추가</button>
         </div>
     )
 }
