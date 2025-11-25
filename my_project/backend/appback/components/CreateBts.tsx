adb284cc387c0952d797b8e5f8ce050ceb5f859e
yj-leeee <asas87189@gmail.com>
2025-11-25 14:05:00 +0900

    버튼 애니메이션까지 완료

--- my_project/backend/appback/components/CreateBts.tsx
+++ my_project/backend/appback/components/CreateBts.tsx
@@ -5,13 +5,31 @@ import { Animated, Pressable, TextInput, View } from "react-native";
 
 export default function CreateBtn(){
     const[open, setOpen] = useState(false);
-    const animation = useRef(new Animated.Value((1))).current;
+    const animation = useRef(new Animated.Value((0))).current;
+
+    const toggle = () => {
+        const toValue = open ? 0 : 1; //true면 0도로, false면 45도로
+
+        Animated.timing(animation, {
+            toValue,
+            duration: 250,
+            useNativeDriver: true,
+        }).start();
+        setOpen(!open);
+    }
+
+    //0 -> 0도 1-> 45도
+    const rotate = animation.interpolate({
+        inputRange: [0,1],
+        outputRange : ['0deg', '45deg'],
+    });
 
     return (
         <View style={createBtn.div}>
             {open && <TodoInput />}
-            <Pressable style={createBtn.btn} onPress={() => {setOpen(!open)}}>
-                <Animated.View>
+            <Pressable style={createBtn.btn} 
+            onPress={toggle}>
+                <Animated.View style={{ transform: [{rotate}]}}>
                     <Feather name="plus" size={24} color="white" />
                 </Animated.View>
             </Pressable>
