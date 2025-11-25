2061742aeae7425cde8aacea79ff74bff3e82a09
yj-leeee <asas87189@gmail.com>
2025-11-25 13:13:52 +0900

    Todo 백엔드 앱 연결하기 앱 만들기 시작

---
+++ my_project/backend/appback/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+
+
+export default function RootLayout() {
+
+  return (
+    <SafeAreaProvider>
+      <Stack />
+    </SafeAreaProvider>
+  );
+}
---
+++ my_project/backend/appback/app/index.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Home(){
+    return(
+        <SafeAreaView style={{flex:1}}>
+
+        </SafeAreaView>
+    )
+}
