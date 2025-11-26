4fe4b5ecd676a17d631ee6f21aad7967d7686c5d
yj-leeee <asas87189@gmail.com>
2025-11-26 11:47:23 +0900

    스프링에 앱도 연결할 수 있도록 추가

--- backend/src/main/java/com/example/backend/list/TodoController.java
+++ backend/src/main/java/com/example/backend/list/TodoController.java
@@ -2,6 +2,7 @@ package com.example.backend.list;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "*")
 @RequestMapping("/todo")
 @RestController
 @RequiredArgsConstructor
